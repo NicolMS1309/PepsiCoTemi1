78b46f975b9dac9a5a90d3baf4d8be4557aff9d0
Nicol Mejía Segura <98182372+NicolMS1309@users.noreply.github.com>
2022-04-22 14:00:52 -0500

    +Se genero pantalla completa, se agrego la clase para que detecte personas

--- app/src/main/java/com/example/pepsicotemi1/MainActivity.java
+++ app/src/main/java/com/example/pepsicotemi1/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.pepsicotemi1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pepsicotemi1.Clases.Deteccion;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Deteccion deteccion;
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            View view = getWindow().peekDecorView();
+            view.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        deteccion = new Deteccion(this, this);
+
+        deteccion.addListener();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        deteccion.removeListener();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
